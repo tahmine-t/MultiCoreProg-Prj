ea4e0aae0db5830fb9125aa1e1acee381dcdc063
Alireza <you@example.com>
2024-05-17 16:05:07 +0330

    Save the results in txt file

--- openMp/n_body_omp.cpp
+++ openMp/n_body_omp.cpp
@@ -6,31 +6,50 @@
 #include "CycleTimer.h"
 #include <omp.h>
 
+
+#include <fstream>
+
 using namespace std;
 
 // Compute forces on each body with time step
 
-void display_bodies()
-{
-  for( int i = 0; i < BODY_COUNT; i++ ) 
-  {
+void display_bodies(ofstream& outfile){
+  for( int i = 0; i < BODY_COUNT; i++ ) {
     printf("\nBody %d:\nMass: %f\nPosition(x ,y, z): %f, %f, %f\nVelocity(x, y, z): %f, %f, %f\nAcceleration(x ,y, z): %f, %f, %f\n\n",
       i + 1, 
       nBodyMass[i], 
       nBodyPosition[i].x, nBodyPosition[i].y, nBodyPosition[i].z,
       nBodyVelocity[i].x, nBodyVelocity[i].y, nBodyVelocity[i].z,
       nBodyAcceleration[i].x, nBodyAcceleration[i].y, nBodyAcceleration[i].z);
+    
+    outfile << "Body " << i + 1 << ":" << endl;
+    outfile << "Mass: " << nBodyMass[i] << endl;
+    outfile << "Position(x, y, z): "
+            << nBodyPosition[i].x << " " << nBodyPosition[i].y << " " << nBodyPosition[i].z << endl;
+    outfile << "Velocity(x, y, z): "
+            << nBodyVelocity[i].x << " " << nBodyVelocity[i].y << " " << nBodyVelocity[i].z << endl;
+    outfile << "Acceleration(x, y, z): "
+            << nBodyAcceleration[i].x << " " << nBodyAcceleration[i].y << " " << nBodyAcceleration[i].z << endl << endl;
   }
 }
 
-void compute() 
-{
+
+void compute(){
   double start, end, minParallel = 1e30;
-  for (int j = 0; j < 2; ++j)
-  {
+  
+  ofstream in_file("nbody_input_data.txt");  // Open file for writing
+  if (!in_file.is_open()) {
+    cerr << "Error opening file!" << endl;
+    return;
+  }
+  display_bodies(in_file);
+  in_file.close();  // Close the file
+
+
+
+  for (int j = 0; j < 2; ++j){
     start = CycleTimer::currentSeconds();
-    for (int i = 0; i < 100; ++i)
-     {
+    for (int i = 0; i < 100; ++i){
        updatePhysics(i * 100);
      } 
     
@@ -40,6 +59,14 @@ void compute()
     // previousTime = currentTime;
 
   }
+
+  ofstream out_file("nbody_output_data.txt");  // Open file for writing
+    if (!out_file.is_open()) {
+    cerr << "Error opening file!" << endl;
+    return;
+  }
+  display_bodies(out_file);
+  out_file.close();  // Close the file
   printf("Time Taken by Parallel implementation: %f ms\n", (minParallel)*1000);
 }
 
@@ -52,8 +79,7 @@ void updateAcceleration(int bodyIndex )
 
   for( int i = 0; i < BODY_COUNT; i++ ) 
   {
-    if( i == bodyIndex ) 
-    {
+    if( i == bodyIndex){
       continue;
     }
 
